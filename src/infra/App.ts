4e65856f3bbb4083bcc57282d69aa8174cfd9efe
Emanuel3k <emanuelsantos_677@hotmail.com>
2024-10-11 23:44:33 -0300

    feat: enable cors and body validation

--- src/infra/App.ts
+++ src/infra/App.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "@/infra/App.module";
+import { ValidationPipe } from "@nestjs/common";
 
 class App {
   async server() {
     const app = await NestFactory.create(AppModule, {
       logger: ["error", "warn", "log"],
     });
-
+    
     app.setGlobalPrefix("api");
+    app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
 
     await app.listen(1337);
   }
